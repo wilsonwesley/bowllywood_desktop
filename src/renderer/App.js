35dfc5ef5a543844154fa9f6941e12b6703166b5
wilsonwesley <wesley-wilson@outlook.com>
2023-01-25 15:51:27 +0100

    Factorisation de l'app.js

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,51 +1,16 @@
-import { useEffect, useState } from "react";
-import logo from "../logo.svg";
 import "./App.css";
 import AddFranchiseRequestScreen from "./addFranchiseRequest/AddFranchiseRequestScreen";
 import RestaurantsListScreen from "./restaurantsList/RestaurantsList";
 import StocksListScreen from "./stocksList/StocksListScreen";
-
-const titleText = {
-  0: "Welcome !",
-  1: "It's up to you!",
-};
+import StocksDetailScreen from "./stocksDetail/StocksDetailScreen";
 
 function App() {
-  const [title, setTitle] = useState(0);
-  const handleClick = () => {
-    window.electron.send("get-new-title", title);
-  };
-  useEffect(() => {
-    window.electron.recieve("display-new-title", (data) => {
-      setTitle(data);
-    });
-  }, [title]);
-
   return (
-    <div className="App">
-      {/* <AddFranchiseRequestScreen /> */}
+    <div>
+      {/*<AddFranchiseRequestScreen /> */}
       {/* <RestaurantsListScreen /> */}
       <StocksListScreen />
-      {/* <header className="App-header">
-        <img id="logo" src={logo} className="App-logo" alt="logo" />
-        <div className="title">
-          <h1>{titleText[title]}</h1>
-          <img src="./potion.svg" className="App-potion" alt="potion" />
-        </div>
-        <p>Official Boilerplate for React Electron App</p>
-        <a
-          title="Site La Manu"
-          className="App-link"
-          href="https://lamanu.fr"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit La Manu
-        </a>
-        <button onClick={handleClick} id="changeTitle">
-          Change Title
-        </button>
-      </header> */}
+      {/* <StocksDetailScreen /> */}
     </div>
   );
 }
