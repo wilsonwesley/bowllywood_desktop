78c61600c16d9945fa2b09fcef895f2debdfe071
wilsonwesley <wesley-wilson@outlook.com>
2023-01-25 10:15:03 +0100

    Retrieve stock list feature

---
+++ src/renderer/stocksList/StocksListScreen.jsx
@@ -0,0 +1,80 @@
+import { getAllstock } from './../../services/stocks';
+import { useEffect, useState } from 'react';
+
+import { Col, Row, Container } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+
+
+const StocksListScreen = () => {
+
+    const [allStocks, setAllStocks] = useState([]);
+    useEffect(() => {
+        getAllstock()
+            .then((res) => {
+                setAllStocks(res.data);
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    return (
+        <Container>
+            <Row>
+                <Col className='col-12 flex-center'>
+                    <img
+                        src="bowllywood.png"
+                        alt="Logo du restaurant de bowls nommé Bowllywood"
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Référence</th>
+                                <th>Produit</th>
+                                <th>Saveur</th>
+                                <th>Quantité</th>
+                                <th>Seuil limite</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {allStocks.map(
+                                (item) => {
+                                    return (
+                                        <tr
+                                            key={item._id}>
+                                            <td>
+                                                {item.ref}
+                                            </td>
+                                            <td>
+                                                {item.name}
+                                            </td>
+                                            <td>
+                                                {item.category}
+                                            </td>
+                                            <td>
+                                                {item.quantity}
+                                            </td>
+                                            <td>
+                                                {item.quantityLimit}
+                                            </td>
+                                            <td>
+                                                {item.status}
+                                            </td>
+                                        </tr>
+                                    );
+                                }
+                            )}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+export default StocksListScreen;
