2e7fcdf9b64151ab967470bc135f7e14014b83af
wilsonwesley <wesley-wilson@outlook.com>
2023-01-27 13:16:12 +0100

    Set up franchise requests list feature

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddFranchiseRequestScreen from "./addFranchiseRequest/AddFranchiseRequestScreen";
 import HomeScreen from "./Home/HomeScreen";
 import KitchenCalendar from "./kitchenCalendar/KitchenCalendar";
+import FranchiseRequestsList from "./franchiseRequestsList";
 import RestaurantsListScreen from "./restaurantsList/RestaurantsList";
 import StocksListScreen from "./stocksList/StocksListScreen";
 import StocksDetailScreen from "./stocksDetail/StocksDetailScreen";
@@ -26,12 +27,14 @@ function App() {
               path="/stocks/extract/:id"
               element={<StocksExtractScreen />}
             />
+            <Route
+              path="/franchiseRequestsList"
+              element={<FranchiseRequestsList />}
+            />
           </Route>
         </Routes>
       </Router>
       {/* <StocksDetailScreen /> */}
-      {/* <StocksSupplyScreen /> */}
-      {/* <StocksExtractScreen /> */}
     </div>
   );
 }
