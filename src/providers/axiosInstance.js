d71377c0c9025fd58803f672fcba96f3da56cd93
Lucile <sirius_86@outlook.fr>
2023-03-22 12:03:55 +0100

    recup dev

--- src/providers/axiosInstance.js
+++ src/providers/axiosInstance.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://bowllywood.onrender.com/",
+  baseURL: "https://bowllywood-8llo.onrender.com/",
   // baseURL: "http://localhost:5000/",
-  // baseURL: 'http://localhost:3000/'
 });
 
 axiosInstance.interceptors.request.use(
