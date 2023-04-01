5489e4c0e575793edbf3f5a800136f3936e694dd
Lucile <sirius_86@outlook.fr>
2023-04-01 17:46:51 +0200

    Protect Home links & cmall correction into error and maint. pages

--- src/renderer/Home/HomeScreen.jsx
+++ src/renderer/Home/HomeScreen.jsx
@@ -1,88 +1,94 @@
-/*
-Icon vert qd on est sur la page en question
-*/
-
 import './HomeScreen.scss'
-import { useState/*, useEffect*/ } from 'react';
+import { useState, useEffect } from 'react';
 import { ListGroup } from 'react-bootstrap';
-// import { getCurrentUserDetails } from '../../services/users' ;
+import { getCurrentUserDetails } from '../../services/users' ;
 import ThinHeader from '../../components/ThinHeader';
 import HomeListItem from '../../components/HomeListItem';
 import IconNavigation from '../../components/IconNavigation';
+import jwt_decode from "jwt-decode";
 
 function HomeScreen() {
-	const [user/*, setUser*/] = useState({});
+	const [userFullname, setUserFullname] = useState(''),
+		[role, setRole] = useState('');
+
+	let linkList = [],
+	 	navList = [];
 
 	// get user details for names 
-	/* useEffect(()=>{
-		getCurrentUserDetails().then((res)=>{
-			setUser(res.data)
-		}).catch((err)=>{
-			console.log('HOME : GET USER DETAILS', err)
-		})
-	}, [user]) */
+	useEffect(()=>{
+		const tokens = localStorage.getItem("userTokens"),
+			userToken = jwt_decode(JSON.parse(tokens).token);
+			setRole(userToken?.roleID)
 
-	// [ en attendant de pouvoir se connecter
-	user.lastName = 'Jean';
-	user.firstName = 'Hertat';
-	const userFullname = `${user.lastName} ${user.firstName}`;
-	// ]
+		if (userToken?.roleID !== 'ROLE_USER')
+		{
+			getCurrentUserDetails(userToken?.id).then((res)=>{
+				setUserFullname(`${res?.data?.data?.firstName ?? ''} ${res?.data?.data?.lastName ?? ''}`);
+			})			
+		}
+	}, [])
 
-	const linkList = [
-		{
-			title: 'Planning employés',
-			isDisplayed: 'true',
-			description: 'Afficher et gérer l’emploi du temps des cuisiniers et des serveurs.',
-			route: '/kitchenCalendar'
-		},
-		{
-			title: 'Les réservations',
-			isDisplayed: 'true',
-			description: 'Consulter et gérer les réservations clients du restaurant.',
-			route: '/reservations'
-		},
-		{
-			title: 'Les stocks',
-			isDisplayed: 'true',
-			description: '[cuisiniers ET/OU chef cuisiniers] Informer un changement de stocks. Ajouter ou retirer.',
-			route: '/stocks'
-		},
-		{
-			title: 'Franchises',
-			isDisplayed: 'true',
-			description: '[PDG] Consulter et valider les demandes de franchises.',
-			route: '/franchiseRequestsList'
-		}/*,
-		{
-			title: 'Restaurants',
-			isDisplayed: 'true',
-			description: '[PDG] Consulter les restaurants',
-			route: '/franchiseRequestsList'
-		}*/
-	]
-
-	const navList = [
-	{
+	// set links
+	const kitchenCalendar = {
+		title: 'Agenda employés',
+		isDisplayed: 'true',
+		description: 'Afficher et gérer l’emploi du temps des cuisiniers et des serveurs.',
+		route: '/kitchenCalendar'
+	},
+	reservations = {
+		title: 'Réservations',
+		isDisplayed: 'true',
+		description: 'Consulter et gérer les réservations clients du restaurant.',
+		route: '/reservations'
+	},
+	stocks = {
+		title: 'Inventaire',
+		isDisplayed: 'true',
+		description: '[cuisiniers ET/OU chef cuisiniers] Informer un changement de stocks. Ajouter ou retirer.',
+		route: '/stocks'
+	},
+	franchise = {
+		title: 'Franchises',
+		isDisplayed: 'true',
+		description: 'Consulter et valider les demandes de franchises.',
+		route: '/franchiseRequestsList'
+	},
+	profilIcon = {
 		icon: 'fa-solid fa-user',
 		text: 'Profil',
 		route: '/profile'
 	},
-	{
+	deconnIcon = {
 		icon: 'fa-solid fa-right-from-bracket',
 		text: 'Déconnexion',
 		route: '/'
-	},
-	]
+	}
+
+	if (role === 'ROLE_ADMIN' || role === 'ROLE_SUPERADMIN' || role === 'ROLE_CEO') {
+		linkList = [kitchenCalendar, reservations, stocks, franchise]
+		navList = [profilIcon, deconnIcon]
+	}
+	else if (role === 'ROLE_MANAGER' || role === 'ROLE_WAITER' || role === 'ROLE_COOK') {
+		linkList = [kitchenCalendar, reservations, stocks] 
+		navList = [profilIcon, deconnIcon]
+	}
+	else {
+		navList = [deconnIcon]
+	}
 
 	return (
 		<div className="homeScreen d-flex flex-column justify-content-between vh-100 px-5 py-4">	
 			<ThinHeader subTitle={`Bonjour ${userFullname}`} />
 
-			<ListGroup className="homeListGroup container-fluid">
-				{linkList.map((link, index)=>{
-					return <HomeListItem key={index} title={link.title} isDisplayed={link.isDisplayed} description={link.description} route={link.route} index={index} />
-				})}
-			</ListGroup>
+			{
+				(linkList.length !== 0)
+				? <ListGroup className="homeListGroup container-fluid">
+					{linkList.map((link, index)=>{
+						return <HomeListItem key={index} title={link.title} isDisplayed={link.isDisplayed} description={link.description} route={link.route} index={index} />
+					})}
+				</ListGroup>
+				: <p className="m-0 text-center">Il semblerait que vous n'ayez pas les droits pour accéder aux informations.</p>	
+			}
 
 			<IconNavigation iconArr={navList} />
 		</div>
